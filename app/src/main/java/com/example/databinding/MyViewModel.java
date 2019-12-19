ff37c563922129b30befc0c37597a0de4fc1dfb3
kangshengjun <qq1536487825@gmail.com>
2019-12-19 14:31:41 +0800

    DataBind

---
+++ app/src/main/java/com/example/databinding/MyViewModel.java
@@ -0,0 +1,22 @@
+package com.example.databinding;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MyViewModel extends ViewModel {
+    private MutableLiveData<Integer> number;
+
+
+    public MutableLiveData<Integer> getNumber() {
+        if (number ==null){
+            number = new MutableLiveData<>();
+            number.setValue(0);
+        }
+        return number;
+    }
+    //功能函数
+    public void add(){
+        number.setValue(number.getValue()+1);
+
+    }
+}
